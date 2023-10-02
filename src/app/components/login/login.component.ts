638c532750b3a5cf65c44da54b549fb7f9ab5cf7
Tristan Bierig <112637018+TristanBierig@users.noreply.github.com>
2023-10-02 17:19:37 +0200

    Basic Form validation

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,11 +7,26 @@ import { FormBuilder } from '@angular/forms';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-  
+  get email() {
+    return this.loginForm.get('email');
+  }
+  get password() {
+    return this.loginForm.get('password');
+  }
+
   constructor(private fb: FormBuilder) {}
 
   loginForm = this.fb.group({
-    email: [''],
-    password: [''],
-  })
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+  });
+
+  getEmailErrorMsg(): string {
+    if (this.loginForm.get('email')!.errors?.['required']) {
+      const errMsg = 'Please enter an email';
+      return errMsg;
+    }
+    const errMsg = 'Please enter a valid email address';
+    return errMsg;
+  }
 }
